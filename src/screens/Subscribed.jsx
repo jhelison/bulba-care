756a34a1fb9cf3f6f37460be4ed810d0313134f1
Jhelison <jhelisong@gmail.com>
2021-07-18 16:22:03 -0300

    Added the subscribed page

--- src/screens/Subscribed.jsx
+++ src/screens/Subscribed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import './Subscribed.css'
+import React from "react"
+import "./Subscribed.css"
 
 const Subscribed = (props) => {
     return (
-        <h1>Sub!</h1>
+        <div className="subscribed-content d-flex align-items-center just-content-center flex-column">
+            <img
+                src="https://www.pikpng.com/pngl/b/220-2209451_ivysaur-gen-ivysaur-gen-1-sprite-clipart.png"
+                alt="bulbasaur-gen1"
+            />
+            <div className="d-flex align-items-center just-content-center flex-column" style={{zIndex:2}}>
+                <h1>Obrigado,</h1>
+                <h1>Jhelison!</h1>
+                <div className="subtitle mt-20">
+                    <span>
+                        Não se procupe, você recebera no email
+                        jhelisong@gmail.com as melhores promoções do mercado!
+                    </span>
+                </div>
+                <div className="price-card-button bg-blue2 mt-20 d-flex just-content-center align-items-center">
+                    <span className="price-sub">Voltar</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
