70ec2feed23fd30bb764e960f761b561a749db75
Jhelison <jhelisong@gmail.com>
2021-07-25 12:11:57 -0300

    Started customers

--- src/components/template/Dashboard/Content.jsx
+++ src/components/template/Dashboard/Content.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Content.css'
+import React from "react"
+import "./Content.css"
 
 const Content = (props) => {
     return (
-        <div className="dash-content">
+        <div className="dash-content d-flex flex-column" style={{alignItems:"flex-start"}}>
             <h2>{props.title}</h2>
             <hr className="solid mt-40"></hr>
-            {props.content}
+            <div className="props-content mt-40">{props.children}</div>
         </div>
     )
 }
